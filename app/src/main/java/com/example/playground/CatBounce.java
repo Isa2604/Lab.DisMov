a07335447c08bcce3ba0d91c973f95f175106118
JuanMi <llanas21.jm@gmail.com>
2024-05-13 22:18:27 -0600

    Modifique contador de puntuacion más alta

--- app/src/main/java/com/example/playground/CatBounce.java
+++ app/src/main/java/com/example/playground/CatBounce.java
@@ -7,8 +7,12 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class CatBounce extends AppCompatActivity {
 
@@ -23,16 +27,56 @@ public class CatBounce extends AppCompatActivity {
     private final float GRAVITY = 1.0f; // Gravedad aplicada a la pelota
 
     private final int FLOOR_HEIGHT_OFFSET = 170; // Offset de altura para el suelo
+    private int highScore = 0; // Variable para almacenar el puntaje máximo
+    private TextView highScoreTextView; // Declaración de la variable TextView para el puntaje máximo
+    private BottomNavigationView bottomNavigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.catbounce_layout);
 
+
         getSupportActionBar().hide();
+
+        bottomNavigationView = findViewById(R.id.nav_view);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_notifications:
+                        // Acción para el ítem de notificaciones (sonido)
+                        // Agrega aquí el código que deseas ejecutar cuando se haga clic en el ítem de notificaciones
+                        Toast.makeText(CatBounce.this, "Sonido seleccionado", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.navigation_home:
+                        // Acción para el ítem de inicio (home)
+                        // Agrega aquí el código que deseas ejecutar cuando se haga clic en el ítem de inicio
+                        Toast.makeText(CatBounce.this, "Inicio seleccionado", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.navigation_dashboard:
+                        // Acción para el ítem de dashboard (ajustes)
+                        // Agrega aquí el código que deseas ejecutar cuando se haga clic en el ítem de ajustes
+                        Toast.makeText(CatBounce.this, "Ajustes seleccionados", Toast.LENGTH_SHORT).show();
+                        return true;
+                }
+                return false;
+            }
+        });
 
         ballImageView = findViewById(R.id.ball);
         scoreTextView = findViewById(R.id.scoreTextView);
+
+
+
+
+        // Dentro de onCreate después de inicializar los elementos de la interfaz de usuario
+        highScoreTextView = findViewById(R.id.highScoreTextView);
+        updateHighScore(); // Actualizar el TextView con el puntaje máximo inicial
+
+
 
         ballImageView.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -49,11 +93,20 @@ public class CatBounce extends AppCompatActivity {
                     ballSpeedY -= score * 0.7; // Aumentar la velocidad vertical con cada toque
                     return true;
                 }
+                if (score > highScore) {
+                    highScore = score; // Actualizar el puntaje máximo si el puntaje actual es mayor
+                    updateHighScore(); // Actualizar el TextView del puntaje máximo
+                }
+
                 return false;
             }
         });
 
         handler = new Handler();
+    }
+
+    private void updateHighScore() {
+        highScoreTextView.setText(""+highScore);
     }
 
     @Override
@@ -90,9 +143,12 @@ public class CatBounce extends AppCompatActivity {
         // Calcular nueva posición de la pelota
         float newX = ballImageView.getX() + ballSpeedX;
         float newY = ballImageView.getY() + ballSpeedY;
-
         // Verificar si la pelota ha tocado los bordes horizontales
-        if (newX <= 0 || newX >= getWindow().getDecorView().getWidth() - ballImageView.getWidth()) {
+        if (newX <= 0) {
+            newX = 0; // Asegurarse de que la pelota no se salga del borde izquierdo
+            ballSpeedX = -ballSpeedX; // Invertir la dirección horizontal
+        } else if (newX >= getWindow().getDecorView().getWidth() - ballImageView.getWidth()) {
+            newX = getWindow().getDecorView().getWidth() - ballImageView.getWidth(); // Asegurarse de que la pelota no se salga del borde derecho
             ballSpeedX = -ballSpeedX; // Invertir la dirección horizontal
         }
 
@@ -114,6 +170,9 @@ public class CatBounce extends AppCompatActivity {
         // Actualizar la posición de la pelota
         ballImageView.setX(newX);
         ballImageView.setY(newY);
+
+        // Agregar rotación a la pelota
+        ballImageView.setRotation(ballImageView.getRotation() + 3); // Cambia el ángulo de rotación gradualmente
     }
 
     private void showGameOverMessage() {
